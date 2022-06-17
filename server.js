0794e2c94d0955bcd924b98219bc297336199caf
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-06-17 20:10:26 +0700

    first commit
    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs')
+
+const names = ["francis","james","Rob"]
+const cars = {
+    name:"Ford",
+    model:"Fiesta"
+}
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        let HTML = fs.readFileSync(`${__dirname}/index.html`)
+        res.end(HTML);
+    } else if(req.url === '/api/user') {
+        res.writeHead(200,{'Content-Type':'application/json'});
+        const json = JSON.stringify({
+            names,
+            cars
+        })
+        res.end(json);
+    } else {
+        res.writeHead(404);
+        res.end()
+    }
+})
+
+
+server.listen(8181,'127.0.0.1')
+console.log('Server is listening on port 8181')
